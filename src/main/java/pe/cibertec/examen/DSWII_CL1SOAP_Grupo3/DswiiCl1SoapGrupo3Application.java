5e8a1090305d4fa68b45830c6efe990beea04a08
MadAlexO <aluyo_2012@hotmail.com>
2024-06-08 14:06:34 -0500

    Pregunta01

---
+++ src/main/java/pe/cibertec/examen/DSWII_CL1SOAP_Grupo3/DswiiCl1SoapGrupo3Application.java
@@ -0,0 +1,13 @@
+package pe.cibertec.examen.DSWII_CL1SOAP_Grupo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswiiCl1SoapGrupo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswiiCl1SoapGrupo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/cibertec/examen/DSWII_CL1SOAP_Grupo3/DswiiCl1SoapGrupo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.cibertec.examen.DSWII_CL1SOAP_Grupo3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswiiCl1SoapGrupo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
